db9af4c5418a267ffd25ff74f4eaf638e4abb2af
Rita <162321842+faceless-void666@users.noreply.github.com>
2024-07-21 12:16:29 +0300

    Create a function that reverses a string
    Use function expressions as foundation for creating three functions each
    responsible for one action.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+  let splitString = function(str) {
+    let result = str.split("");
+    return result;
+  }
+  
+    let reverseArray = function(str) {
+    let result = str.reverse();
+    return result;
+  }
+    let reversedArray = reverseArray(splitString(str));
+    let endResult = reversedArray.join("");
+    return endResult;
 };
 
 // Do not edit below this line
