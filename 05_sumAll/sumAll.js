e3f399ec557e14e9a04a1eb6caa8f5d063585770
faceless-void666 <162321842+faceless-void666@users.noreply.github.com>
2024-07-22 18:30:20 +0300

    Create a function that sums integers in a range
    Function takes 2 positive integers and sums all numbers between those 2.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let max = Math.max(num1, num2);
+  let min = Math.min(num1, num2);
+  let range = [];
+  let sum = 0;
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  } else if (isNaN(num1) || isNaN(num2)) {
+    return "ERROR";
+  }
+   else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+   }
+    for (let i = min; i <= max; i++) {
+      range.push(i);
+    }
 
+    for (let j = 0; j < range.length; j++) {
+      sum += range[0] + j;
+    }
+    return sum;
 };
 
 // Do not edit below this line
